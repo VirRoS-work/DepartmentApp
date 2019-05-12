f8c50eeec3bed19d6c6ca7b99579932ca97326af
Virros <virros.work@gmail.com>
2019-05-12 10:40:17 +0400

    - Add swagger in project

--- src/main/java/com/virros/department/controllers/DepartmentController.java
+++ src/main/java/com/virros/department/controllers/DepartmentController.java
@@ -3,20 +3,22 @@ package com.virros.department.controllers;
 import com.virros.department.model.dto.DepartmentDto;
 import com.virros.department.services.DepartmentService;
 import com.virros.department.validation.exceptions.EntityNotFoundException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/departments")
+@Api(value = "Департаменты", description = "Контроллер обеспечивает работу с департаментами" )
 public class DepartmentController {
 
     @Autowired
     DepartmentService service;
 
-    //D4
-
     @GetMapping(value = "/{id}")
+    @ApiOperation(value = "D4 / Просмотр сведений о департаменте по его id.")
     public ResponseEntity<?> getInformationAboutDepartment(@PathVariable Integer id) throws EntityNotFoundException {
 
         DepartmentDto result = service.getDepartmentDto(id)
